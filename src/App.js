98383420c7fcb551ed014dae948e5225ee0560b9
Irene R <irene@smallwondermedia.com>
2019-03-21 22:57:39 -0400

    data request ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 class App extends Component {
+
+    state = {
+        metroData: []
+    }
+
+    callMetro = () => {
+        axios.get('https://api.wmata.com/StationPrediction.svc/json/GetPrediction/B03', {
+            headers: {
+                "api_key": String(process.env.REACT_APP_METRO_KEY)
+            }
+        })
+        .then(res => {
+            this.setState({
+                metroData: res.data.Trains
+            })
+            console.log(this.state.metroData);
+        })
+    }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <button onClick={this.callMetro}>Submit</button>
+
       </div>
     );
   }
