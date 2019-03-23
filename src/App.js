1887b5ebaf8eaf8cbaca0dcbc849216d8ef810e7
Irene R <irene@smallwondermedia.com>
2019-03-23 18:19:43 -0400

    trains in console log but not getting ids

--- src/App.js
+++ src/App.js
@@ -10,6 +10,20 @@ class App extends Component {
         trainId: ""
     }
 
+    // callMetro = () => {
+    //     axios.get('https://api.wmata.com/TrainPositions/TrainPositions?contentType=json', {
+    //         headers: {
+    //             "api_key": String(process.env.REACT_APP_METRO_KEY)
+    //         }
+    //     })
+    //     .then(res => {
+    //         this.setState({
+    //             metroData: res.data.TrainPositions,
+    //         })
+    //         console.log(this.state.metroData);
+    //     })
+    // }
+
     callMetro = () => {
         axios.get('https://api.wmata.com/TrainPositions/TrainPositions?contentType=json', {
             headers: {
@@ -21,6 +35,13 @@ class App extends Component {
                 metroData: res.data.TrainPositions,
             })
             console.log(this.state.metroData);
+            // works to here
+
+            let trainId = this.state.metroData.map(train => {
+                console.log(trainId); //undefined
+                console.log(train); //return each train
+                // need to map through each returned train to get to TrainId
+            })
         })
     }
 
@@ -31,10 +52,9 @@ class App extends Component {
 
         <button onClick={this.callMetro}>Submit</button>
 
-
-        {this.state.metroData.map(trainId => (
+        {this.state.metroData.map(index => (
             <Trains
-                key={trainId}
+                key={index}
             />
         ))}
 
